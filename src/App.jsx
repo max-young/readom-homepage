80a55f54052c37440cde8fffce790a399005bf6a
Max <alvysurvey@gmail.com>
2025-02-12 09:00:11 +0800

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,37 @@
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import { useTranslation } from "react-i18next";
 import i18n from "i18next";
-import { Button } from "@headlessui/react";
+// import { Button } from "@headlessui/react";
+import { FcAndroidOs } from "react-icons/fc";
 
 function App() {
   const { t } = useTranslation();
 
   return (
-    <div className="container flex flex-col items-center justify-center min-h-screen py-2">
-      <div>
-        <h1>{t("welcome")}</h1>
-      </div>
-      <div>
-        <img src={viteLogo} alt="Vite logo" />
-      </div>
-      <div className="card">
-        <Button
+    <div className="container max-w-xl space-y-4">
+      <div className="my-8 flex items-end">
+        <h1>Readom</h1>
+        <button
+          className="btn btn-xs mx-4"
           onClick={() => {
             i18n.changeLanguage(i18n.language === "en" ? "zh" : "en");
           }}
         >
           {i18n.language === "en" ? "中文" : "English"}
-        </Button>
+        </button>
+      </div>
+      <div className="text-left space-y-2">
+        <p>{t("about 1")}</p>
+        <p>{t("about 2")}</p>
+        <p>{t("about 3")}</p>
+        <p>{t("about 4")}</p>
+        <p>{t("about 5")}: alvysurvey@gmail.com</p>
+      </div>
+      <div className="text-left space-y-2">
+        <p className="font-bold">{t("download")}:</p>
+        <a href="https://github.com/max-young/readom-homepage/releases/download/v1.0.0/readom-latest.apk">
+          <FcAndroidOs size={32} />
+        </a>
       </div>
     </div>
   );
